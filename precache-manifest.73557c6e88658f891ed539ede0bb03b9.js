6022dd62ca5c7b2b6c36efdac8545ace6de3a252
Fatima Yousif <fyousif30@gmail.com>
2021-01-03 02:35:13 +0500

    Updates

---
+++ precache-manifest.73557c6e88658f891ed539ede0bb03b9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "574328073aa6681ff036b43788c42785",
+    "url": "/Calendar-Buzzer/index.html"
+  },
+  {
+    "revision": "e22c79ba2dda907b7fb5",
+    "url": "/Calendar-Buzzer/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "7c3c6386b00071bf9bec",
+    "url": "/Calendar-Buzzer/static/css/main.5043496d.chunk.css"
+  },
+  {
+    "revision": "e22c79ba2dda907b7fb5",
+    "url": "/Calendar-Buzzer/static/js/2.cb04f4f0.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Calendar-Buzzer/static/js/2.cb04f4f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c3c6386b00071bf9bec",
+    "url": "/Calendar-Buzzer/static/js/main.20c86aeb.chunk.js"
+  },
+  {
+    "revision": "aef8882f235b355c8d06",
+    "url": "/Calendar-Buzzer/static/js/runtime-main.3c3b1ef0.js"
+  },
+  {
+    "revision": "edd20e445da445316dc9f8757d54a70c",
+    "url": "/Calendar-Buzzer/static/media/logo1.edd20e44.png"
+  }
+]);
